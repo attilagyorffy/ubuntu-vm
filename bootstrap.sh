ec47e2d776e4348e6a17dabdb993e34836eaf9d5
Attila Györffy <attila@attilagyorffy.com>
2014-01-21 23:50:01 +0100

    Install node from Chris Lea's PPA

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,11 @@ sudo apt-get update
 # Allow installing from PPAs
 sudo apt-get -y install python-software-properties
 
-# Use the Brightbox Experimental PPA to install latest Ruby from
+# Use the Brightbox Experimental and Chris Lea PPAs to install latest stuff from
 sudo apt-add-repository ppa:brightbox/ruby-ng-experimental
+sudo apt-add-repository ppa:chris-lea/node.js
 
 sudo apt-get update
 
-# Install Ruby 2.0 from the Brightbox PPA
-sudo apt-get -y install ruby2.0
+# Install Ruby 2.0 from the Brightbox PPA and the latest Node from Chris Lea's PPA
+sudo apt-get -y install ruby2.0 nodejs
